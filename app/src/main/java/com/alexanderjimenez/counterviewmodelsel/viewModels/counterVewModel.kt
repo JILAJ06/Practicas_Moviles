993fe9cca35068ce739fbf1bbbb54f67c6ab8e09
JILA06 <jilaj.alexander@gmail.com>
2025-11-05 13:32:30 -0600

    ViewModel, firts push

---
+++ app/src/main/java/com/alexanderjimenez/counterviewmodelsel/viewModels/counterVewModel.kt
@@ -0,0 +1,14 @@
+package com.alexanderjimenez.counterviewmodelsel.viewModels
+
+import androidx.compose.runtime.mutableIntStateOf
+
+
+class counterVewModel() {
+    private val _counter = mutableIntStateOf(0)
+    val counter = _counter
+
+    fun add(){
+        _counter.value++
+    }
+
+}
