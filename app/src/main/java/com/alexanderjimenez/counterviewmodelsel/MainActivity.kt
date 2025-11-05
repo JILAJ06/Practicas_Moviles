c47c36080b94502beb3996cbcc37b7ad961bfa66
JILA06 <jilaj.alexander@gmail.com>
2025-11-05 14:20:39 -0600

    changes view model

--- app/src/main/java/com/alexanderjimenez/counterviewmodelsel/MainActivity.kt
+++ app/src/main/java/com/alexanderjimenez/counterviewmodelsel/MainActivity.kt
@@ -3,38 +3,32 @@ package com.alexanderjimenez.counterviewmodelsel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.alexanderjimenez.counterviewmodelsel.ui.theme.CounterViewModelSelTheme
-
-annotation class CounterVewModel()
+import com.alexanderjimenez.counterviewmodelsel.viewModels.counterVewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val counterVewModel: CounterVewModel = CounterVewModel()
+        val counterViewModel: counterVewModel by viewModels()
         setContent {
-        enableEdgeToEdge()
-        setContent {
-            CounterViewModelSelTheme {
+            MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Content(paddingValues = innerPadding)
-
+                    Content(innerPadding, counterViewModel)
                 }
             }
         }
@@ -42,26 +36,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Content(paddingValues: PaddingValues) {
-    var (_counter) = rememberSaveable {
-        mutableIntStateOf(0)
-    }
-
-    Column(modifier = Modifier
-        .padding(paddingValues = paddingValues)
-        .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-
-        Text(text = "Counter: $_counter")
+fun Content(paddingValues: PaddingValues, counterViewModel: counterVewModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text("Counter: ${counterViewModel.counter.intValue}")
         Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = {
-            _counter++
-        }) {
-            Text(text = "+")
+        Button(
+            onClick = {
+                counterViewModel.add()
+            }
+        ) {
+            Text("Add")
         }
     }
-    }
 }
-
-
--- app/src/main/java/com/alexanderjimenez/counterviewmodelsel/viewModels/counterVewModel.kt
+++ app/src/main/java/com/alexanderjimenez/counterviewmodelsel/viewModels/counterVewModel.kt
@@ -1,14 +1,13 @@
 package com.alexanderjimenez.counterviewmodelsel.viewModels
 
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.lifecycle.ViewModel
 
-
-class counterVewModel() {
+class counterVewModel : ViewModel() {
     private val _counter = mutableIntStateOf(0)
     val counter = _counter
 
     fun add(){
         _counter.intValue++
     }
-
 }
